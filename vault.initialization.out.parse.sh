7314a7762be45f1abf596979cee768c7569beec4
howarddeiner <howard.deiner@deinersoft.com>
2019-04-04 14:39:50 -0400

    Initial commit,

--- vault.initialization.out.parse.sh
+++ vault.initialization.out.parse.sh
@@ -5,37 +5,37 @@ rm -rf ./vault/UnsealKey1 ./vault/UnsealKey2 ./vault/UnsealKey3 ./vault/UnsealKe
 while read line
 do
     echo "$line" | grep  "Unseal Key [0-9]\: \(\.*\)" | cut -d: -f2 | xargs > ./.temp
-    if [ -s "./.temp" ]
-        then
-            if [ ! -f "./vault/UnsealKey1" ]; then
-                cp ./.temp ./vault/UnsealKey1
+    if [ $(wc -c < ./.temp) -ge 8 ]; then
+        if [ ! -f "./vault/UnsealKey1" ]; then
+            cp ./.temp ./vault/UnsealKey1
+        else
+            if [ ! -f "./vault/UnsealKey2" ]; then
+                cp ./.temp ./vault/UnsealKey2
             else
-                if [ ! -f "./vault/UnsealKey2" ]; then
-                    cp ./.temp ./vault/UnsealKey2
+                if [ ! -f "./vault/UnsealKey3" ]; then
+                    cp ./.temp ./vault/UnsealKey3
                 else
-                    if [ ! -f "./vault/UnsealKey3" ]; then
-                        cp ./.temp ./vault/UnsealKey3
+                    if [ ! -f "./vault/UnsealKey4" ]; then
+                        cp ./.temp ./vault/UnsealKey4
                     else
-                        if [ ! -f "./vault/UnsealKey4" ]; then
-                            cp ./.temp ./vault/UnsealKey4
-                        else
-                            if [ ! -f "./vault/UnsealKey5" ]; then
-                                cp ./.temp ./vault/UnsealKey5
-                            fi
+                        if [ ! -f "./vault/UnsealKey5" ]; then
+                            cp ./.temp ./vault/UnsealKey5
                         fi
                     fi
-                 fi
-            fi
-fi
+                fi
+             fi
+        fi
+        rm ./.temp
+    fi
 done < vault.initialization.out
 rm ./.temp
 
 while read line
 do
     echo "$line" | grep  "Initial Root Token\: \(\.*\)" | cut -d: -f2 | xargs > ./.temp
-    if [ -s "./.temp" ]
-        then
-            cp ./.temp ./vault/InitialRootToken
-fi
+    if [ $(wc -c < ./.temp) -ge 8 ]; then
+        cp ./.temp ./vault/InitialRootToken
+        rm ./.temp
+    fi
 done < vault.initialization.out
 rm ./.temp
